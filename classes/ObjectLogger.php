ce32250b5cd5c6f41338fc6e9ff32ecca05266d3
Irene <ileon@webimpacto.es>
2019-03-28 16:11:35 +0100

    WEB-1669 Terminado hook

--- classes/ObjectLogger.php
+++ classes/ObjectLogger.php
@@ -2,6 +2,7 @@
 
 class ObjectLogger extends ObjectModel 
 {
+
     public static $definition = array(
         'table' => 'objectlogguer',
         'primary' => 'id_objectlogguer',
@@ -9,9 +10,8 @@ class ObjectLogger extends ObjectModel
             'affected_object' =>        array('type' => self::TYPE_INT, 'validate' => 'isNullOrUnsignedId', 'copy_post' => false),
             'action_type' =>    array('type' => self::TYPE_STRING, 'validate' => 'isGenericName', 'size' => 255),
             'object_type' =>    array('type' => self::TYPE_STRING, 'validate' => 'isGenericName', 'size' => 255),
-            'message' =>        array('type' => self::TYPE_STRING, 'validate' => 'isGenericName', 'size' => 39999999),
+            'message' =>        array('type' => self::TYPE_STRING, 'validate' => 'isGenericName', 'size' => 255),
             'date_add' =>        array('type' => self::TYPE_DATE, 'validate' => 'isDateFormat', 'copy_post' => false),
         ),
     );
 }
-?>
--- wim_objectlogguer.php
+++ wim_objectlogguer.php
@@ -1,107 +1,73 @@
 <?php
+require_once 'classes/ObjectLogger.php';
 if(!defined('_PS_VERSION_'))
 exit;
 class wim_objectlogguer extends Module{
-    public function __construct(){
-        $this->name = 'wim_objectlogguer';
-        $this->tab = 'administration';
-        $this->version = '1.0.0';
-        $this->author ='Irene León';
-        $this->need_instance = 0;
-        $this->ps_version_compliancy = array('min' => '1.6', 'max' => _PS_VERSION_);
-        $this->bootstrap = true;
+  public function __construct() {
+    $this->name = 'wim_objectlogguer';
+    $this->tab = 'administration';
+    $this->version = '1.0.0';
+    $this->author = 'Irene León';
+    $this->need_instance = 0;
+    
+    $this->bootstrap = true;
 
-        parent::__construct();
+    $this->displayName = $this->l('wim_objectlogguer');
+    $this->description = $this->l('Primer Modulo');
+    $this->ps_versions_compliancy = array('min' => '1.6', 'max' => _PS_VERSION_);
 
-        $this->displayName = $this->l('wim_objectlogguer');
-        $this->description = $this->l('Primer modulo');
-    }
+    parent::__construct();
+}
 
-    public function install(){
-        $sql = 'CREATE TABLE IF NOT EXISTS`'._DB_PREFIX_.'objectlogguer`(
-            `id_objectlogguer` int(11) NOT NULL AUTO_INCREMENT,
+public function install() {
+    Db::getInstance()->execute(
+        "CREATE TABLE IF NOT EXISTS `"._DB_PREFIX_."objectlogguer`(
+            `id_objectlogguer` int(11) AUTO_INCREMENT,
             `affected_object` int(11),
             `action_type` varchar(255),
             `object_type` varchar(255),
             `message` text,
-            `date_add`datetime,
+            `date_add` datetime,
             PRIMARY KEY (`id_objectlogguer`)
-        )
-        ENGINE= '. _MYSQL_ENGNE_ .' DEFAULT CHARSET=utf8;';
+        ) ENGINE="._MYSQL_ENGINE_."DEFAULT CHARSET=UTF8;"
+    );
 
-        if(Db::getInstance()->execute($sql) == false){
-            return false;
+    return parent::install()
+            && $this->registerHook('header')
+            && $this->registerHook('backOfficeHeader') 
+            && $this->registerHook('actionObjectAddAfter')
+            && $this->registerHook('actionObjectDeleteAfter')
+            && $this->registerHook('actionObjectUpdateAfter');
+    }
+
+    public function annadirAccion($params, $event) {
+        $accion = new ObjectLogger();
+        $accion->affected_object = $params['object']->id;
+        $accion->action_type = $event;
+        $accion->object_type = get_class($params['object']);
+        if($event == "update" || $event == "delete") {
+            $accion->message = "Object ". get_class($params['object']) . " with id " . $params['object']->id . " was $event" ."d";
+        }else {
+            $accion->message = "Object ". get_class($params['object']) . " with id " . $params['object']->id . " was $event" ."ed";
         }
-        return parent::install()&&
-
-        $this->registerHook('header')&&
-        $this->registerHook('displayhome')&&
-        $this->registerHook('actionObjectAddBefore')&&
-        $this->registerHook('actionObjectAddAfter')&&
-        $this->registerHook('actionObjectUpdateBefore')&&
-        $this->registerHook('actionObjectUpdateAfter')&&
-        $this->registerHook('actionObjectDeleteAfter')&&
-        $this->registerHook('actionObjectDeleteBefore');
-    }
-    public function hookActionObjectDeleteAfter($params) {
-        Db::getInstance()->insert('objectlogguer',array(
-            'affected_object' => $params['object']->id, 
-            'action_type' => "delete",
-            'object_type' =>  get_class($params['object']),
-            'message' => "Object with id " . $params['object']->id . " deleted",
-            'date_add' => date("Y-m-d H:i:s"),
-        ));
+        $accion->date_add = date("Y-m-d H:i:s");
+        if(get_class($params['object']) != "ObjectLogger"){
+            $accion->add();
+        }
     }
 
-    /*public function hookActionObjectDeleteBefore($params) {
-        Db::getInstance()->insert('objectlogguer',array(
-            'affected_object' => $params['object']->id, 
-            'action_type' => "delete",
-            'object_type' =>  get_class($params['object']),
-            'message' => "Object with id " . $params['object']->id . " deleted",
-            'date_add' => date("Y-m-d H:i:s"),
-        ));
-    }*/
-
-    public function hookActionObjectAddAfter($params) {
-        Db::getInstance()->insert('objectlogguer',array(
-            'affected_object' => $params['object']->id, 
-            'action_type' => "add",
-            'object_type' =>  get_class($params['object']),
-            'message' => "Object with id " . $params['object']->id . " added",
-            'date_add' => date("Y-m-d H:i:s"),
-        ));
+    public function hookActionObjectAddAfter($params)
+    {
+        $this->annadirAccion($params, "add");
     }
 
-    /*public function hookActionObjectAddBefore() {
-        Db::getInstance()->insert('objectlogguer',array(
-            'affected_object' => $params['object']->id, 
-            'action_type' => "add",
-            'object_type' =>  get_class($params['object']),
-            'message' => "Object with id " . $params['object']->id . " added",
-            'date_add' => date("Y-m-d H:i:s"),
-        ));
-    }*/
-
-    public function hookActionObjectUpdateAfter($params) {
-        Db::getInstance()->insert('objectlogguer',array(
-            'affected_object' => $params['object']->id, 
-            'action_type' => "update",
-            'object_type' =>  get_class($params['object']),
-            'message' => "Object with id " . $params['object']->id . " updated",
-            'date_add' => date("Y-m-d H:i:s"),
-        ));
+    public function hookActionObjectUpdateAfter($params)
+    {
+        $this->annadirAccion($params, "update");
     }
 
-    
-    /*public function hookActionObjectUpdateBefore($params) {
-        Db::getInstance()->insert('objectlogguer',array(
-            'affected_object' => $params['object']->id, 
-            'action_type' => "update",
-            'object_type' =>  get_class($params['object']),
-            'message' => "Object with id " . $params['object']->id . " updated",
-            'date_add' => date("Y-m-d H:i:s"),
-        ));
-    }*/
-}
-?>
+    public function hookActionObjectDeleteAfter($params)
+    {
+        $this->annadirAccion($params, "delete");
+    }
+} 
