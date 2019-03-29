97b2ddcfa1da2fcad97781379e34c9ddb3a3a935
Irene <ileon@webimpacto.es>
2019-03-29 08:32:51 +0100

    WEB-1669 Cambios estructurales

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-/**
+/*
 * 2007-2017 PrestaShop
 *
 * NOTICE OF LICENSE
@@ -18,24 +18,18 @@
 * versions in the future. If you wish to customize PrestaShop for your
 * needs please refer to http://www.prestashop.com for more information.
 *
-*  @author Irene León>
+*  @author PrestaShop SA <contact@prestashop.com>
 *  @copyright  2007-2017 PrestaShop SA
 *  @license    http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
 *  International Registered Trademark & Property of PrestaShop SA
 */
 
-class ObjectLogger extends ObjectModel
-{
-    public static $definition = array(
-        'table' => 'objectlogguer',
-        'primary' => 'id_objectlogguer',
-        'fields' => array(
-            'affected_object' =>        array('type' => self::TYPE_INT, 'validate' => 'isNullOrUnsignedId', 'copy_post' => false),
-            'action_type' =>    array('type' => self::TYPE_STRING, 'validate' => 'isGenericName', 'size' => 255),
-            'object_type' =>    array('type' => self::TYPE_STRING, 'validate' => 'isGenericName', 'size' => 255),
-            'message' =>        array('type' => self::TYPE_STRING, 'validate' => 'isGenericName', 'size' => 255),
-            'date_add' =>        array('type' => self::TYPE_DATE, 'validate' => 'isDateFormat', 'copy_post' => false),
-        ),
-    );
-}
-?>
+header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
+header("Last-Modified: ".gmdate("D, d M Y H:i:s")." GMT");
+
+header("Cache-Control: no-store, no-cache, must-revalidate");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
+
+header("Location: ../");
+exit;
